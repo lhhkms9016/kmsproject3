4c9550ad5d867fdb3b600c6140c9efb772a8d663
lhhkms9016 <lhhkms1124@gmail.com>
2024-06-23 21:56:21 +0900

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -2,13 +2,12 @@ import streamlit as st
 
 def home():
 
-  st.title("Streamlit URL 링크 추가 예제")
+  st.title("제안관련 링크")
   
   # Markdown을 사용하여 URL 링크 추가
-  st.markdown("[Streamlit 홈페이지](https://streamlit.io)")
+  st.markdown("[회귀분석](https://lhhkms9016-liear-lnrtest-iirnzb.streamlit.app/)")
+
+  # Markdown을 사용하여 URL 링크 추가
+  st.markdown("[시계열분](https://ebbdbn5wtt43hjxmqnfxuy.streamlit.app/)")
   
-  # HTML을 사용하여 URL 링크 추가
-  st.markdown('<a href="https://streamlit.io" target="_blank">Streamlit 홈페이지</a>', unsafe_allow_html=True)
-  
-  # st.write를 사용하여 URL 링크 추가
-  st.write("[Streamlit 홈페이지](https://streamlit.io)")
+
