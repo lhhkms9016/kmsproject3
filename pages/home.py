ac4cf368e4c757da710c4bbeb21b75c4790f0405
lhhkms9016 <lhhkms1124@gmail.com>
2024-06-23 21:50:39 +0900

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -1,12 +1,14 @@
 import streamlit as st
 
-st.title("Streamlit URL 링크 추가 예제")
+def home():
 
-# Markdown을 사용하여 URL 링크 추가
-st.markdown("[Streamlit 홈페이지](https://streamlit.io)")
-
-# HTML을 사용하여 URL 링크 추가
-st.markdown('<a href="https://streamlit.io" target="_blank">Streamlit 홈페이지</a>', unsafe_allow_html=True)
-
-# st.write를 사용하여 URL 링크 추가
-st.write("[Streamlit 홈페이지](https://streamlit.io)")
+  st.title("Streamlit URL 링크 추가 예제")
+  
+  # Markdown을 사용하여 URL 링크 추가
+  st.markdown("[Streamlit 홈페이지](https://streamlit.io)")
+  
+  # HTML을 사용하여 URL 링크 추가
+  st.markdown('<a href="https://streamlit.io" target="_blank">Streamlit 홈페이지</a>', unsafe_allow_html=True)
+  
+  # st.write를 사용하여 URL 링크 추가
+  st.write("[Streamlit 홈페이지](https://streamlit.io)")
